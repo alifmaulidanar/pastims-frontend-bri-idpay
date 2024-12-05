0ad9347eb757a659807ac070c8e50a392c968dac
alifmaulidanar <alifmaulidanr@gmail.com>
2024-12-05 22:00:49 +0700

    Changes to handleLogin and handleLogout to use backend API for database interactions

--- src/auth/userAuth.tsx
+++ src/auth/userAuth.tsx
@@ -1,19 +1,57 @@
 import supabase from '@/utils/supabase';
+const supabaseLocalStorageSession = import.meta.env.VITE_SUPABASE_LOCAL_STORAGE_SESSION;
 
 export const handleLogin = async (email: string, password: string) => {
-  const { data, error } = await supabase.auth.signInWithPassword({ email, password });
-  if (error) {
-    return { error };
+  try {
+    const response = await fetch('http://127.0.0.1:8787/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, password }),
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      console.error('Login failed:', errorData.message);
+      return { error: errorData.message };
+    }
+
+    const data = await response.json();
+
+    if (data) {
+      const { error: sessionError } = await supabase.auth.setSession({ access_token: data.session.access_token, refresh_token: data.session.refresh_token });
+      if (sessionError) {
+        console.error('Error setting session:', sessionError.message);
+      }
+    }
+
+    return { data };
+  } catch (error) {
+    console.error('Network error:', error);
+    return { error: 'Network error or failed to connect to server' };
   }
-  return { data };
 };
 
 export const handleLogout = async () => {
-  const { error } = await supabase.auth.signOut();
-  if (error) {
-    console.error("Error logging out:", error.message);
-  } else {
-    console.log("User logged out successfully");
+  const isConfirmed = window.confirm("Apakah Anda yakin ingin logout?");
+  if (!isConfirmed) return;
+
+  try {
+    const response = await fetch('http://127.0.0.1:8787/logout', {
+      method: 'POST',
+    });
+
+    const res = await response.json();
+    if (!response.ok) {
+      console.error("Error logging out:", res.message);
+    }
+
+    localStorage.removeItem("lastWritten");
+    localStorage.removeItem(supabaseLocalStorageSession);
     window.location.href = '/';
+  } catch (error) {
+    console.error("Error logging out:", error);
+    return { error };
   }
 };
