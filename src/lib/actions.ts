dfd8f3182ecb2e985271a374c9c42dfc74eb1614
alifmaulidanar <alifmaulidanr@gmail.com>
2024-12-06 14:31:29 +0700

    Using backend API to save location to database

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,4 +1,4 @@
-import supabase from "@/utils/supabase";
+// import supabase from "@/utils/supabase";
 
 const canWriteData = () => {
   const lastWritten = localStorage.getItem('lastWritten');
@@ -12,51 +12,51 @@ const canWriteData = () => {
   return true;
 };
 
-export const saveLocationToDatabase = async (userId: string, username: string, latitude: number, longitude: number) => {
-  if (canWriteData()) {
-    const { error } = await supabase
-      .from("locations")
-      .upsert({
-        user_id: userId,
-        username,
-        latitude,
-        longitude,
-      });
-
-    if (error) {
-      console.error("Error updating location:", error.message);
-    }
-  } else {
-    return []
-  }
-};
-
 // export const saveLocationToDatabase = async (userId: string, username: string, latitude: number, longitude: number) => {
 //   if (canWriteData()) {
-//     const response = await fetch('http://127.0.0.1:8787/save-location', {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//       body: JSON.stringify({
+//     const { error } = await supabase
+//       .from("locations")
+//       .upsert({
 //         user_id: userId,
 //         username,
 //         latitude,
 //         longitude,
-//       }),
-//     });
+//       });
 
-//     const data = await response.json();
-
-//     if (response.ok) {
-//       console.log('Location saved:', data);
-//     } else {
-//       console.error('Error saving location:', data.message);
+//     if (error) {
+//       console.error("Error updating location:", error.message);
 //     }
 //   } else {
-//     console.log('Too soon to save location. Please wait 5 minutes.');
+//     return []
 //   }
 // };
+
+export const saveLocationToDatabase = async (userId: string, username: string, latitude: number, longitude: number) => {
+  if (canWriteData()) {
+    const response = await fetch('http://127.0.0.1:8787/save-location', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        user_id: userId,
+        username,
+        latitude,
+        longitude,
+      }),
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      console.log('Location saved:', data);
+    } else {
+      console.error('Error saving location:', data.message);
+    }
+  } else {
+    console.log('Too soon to save location. Please wait 5 minutes.');
+  }
+};
 
 export const getLatestLocationForEachUser = async () => {
   try {
