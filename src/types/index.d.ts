2ae6e3b4be8596058c86b037dba135a70c39738e
alifmaulidanar <alifmaulidanr@gmail.com>
2024-12-07 20:25:51 +0700

    Added types for Profile

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -7,3 +7,15 @@ export interface User {
   password: string;
   phone: string;
 }
+
+// Profile type definition
+export interface Profile {
+  id: integer;
+  user_id: string;
+  email: string;
+  username: string;
+  phone: string | null;
+  role: string;
+  created_at: string;
+  updated_at: string;
+}
